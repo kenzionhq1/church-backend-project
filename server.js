8fbf8b8ff1f8ac4c28334119a68f5b10308cf646
OGUNFOWORA TWINS <OGUNFOWORATWINS@OGUNFOWORA-TWINS-macBOOK-AIR.local>
2025-05-10 00:01:52 +0100

    “done”

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -110,7 +110,5 @@ app.post("/subscribe", async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on https://church-backend-project.onrender.com`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
